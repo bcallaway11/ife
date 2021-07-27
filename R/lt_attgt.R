667513ef350e1e961bf4779db062e858ae55504e
Brantly Callaway <brantly.callaway@uga.edu>
2021-07-26 23:23:42 -0400

    added code for staggered adoption

--- R/lt_attgt.R
+++ R/lt_attgt.R
@@ -4,6 +4,7 @@
 # * We rely on equally spaced periods (though difference doesn't need to be 1)
 # * We use all not-yet-treated observations as the comparison group
 #    (rather than a fixed never-treated group)
+# * This code is a bit of a mess ... 
 #-----------------------------------------------------------------------------
 
 
@@ -21,7 +22,13 @@ lt_attgt <- function(gt_data,
   # base period is the first one in this subset of the data
   base.period <- min(gt_data$period)
   tp <- max(gt_data$period)
-  this.n <- nrow(gt_data)/3 # this is probably going to fail
+  this.n <- nrow(gt_data)/3 
+
+  # pick up covariates in the base period (any period ok if
+  # they are time constant, otherwise pick up ones in base
+  # period
+  # for outcome regression, get pre-treatment values
+  Xpre <- model.frame(xformla, data=subset(gt_data,period==base.period))
   
   # take difference with respect to base period
   this.data <- gt_data %>%
@@ -30,15 +37,37 @@ lt_attgt <- function(gt_data,
     as.data.frame()
   # and drop base period
   this.data <- subset(this.data, period != base.period)
+
   # account for longer gap between periods for post-treatment periods
   tp_gap <- tp - base.period
   this.data$dY_base <- if_else(this.data$name=="pre", this.data$dY_base*tp_gap, this.data$dY_base)
 
-  use_data <- this.data
-  use_data$Y <- use_data$dY_base
+  pre_data <- subset(this.data, name=="pre")
+  post_data <- subset(this.data, name=="post")
 
-  # after "adjusting" data, leverage calls to did
-  attgt <- did_attgt(this.data, xformla, ...)
+  lt_adjusted_Y <- post_data$dY_base - pre_data$dY_base
+  post_data$lt_adjusted_Y <- lt_adjusted_Y
   
-  attgt
+  # merge outcome and covariate data
+  gt_dataX <- cbind.data.frame(post_data[,c("lt_adjusted_Y",
+                                            "D",
+                                            "id",
+                                            "period",
+                                            "name")], Xpre)
+
+  # treatment dummy variable
+  D <- gt_dataX$D
+
+  # call DRDID functions to make the computations;
+  # just like in `did` package
+  gt_dataX <- droplevels(gt_dataX)
+  attgt <- DRDID::drdid_panel(y1=lt_adjusted_Y,
+                              y0=rep(0,length(D)),
+                              D=D,
+                              covariates=model.matrix(xformla,
+                                                      data=gt_dataX),
+                              inffunc=TRUE)
+
+  # return attgt
+  attgt_if(attgt=attgt$ATT, inf_func=attgt$att.inf.func)
 }
---
+++ R/staggered_ife.R
@@ -0,0 +1,51 @@
+#' @title staggered_ife
+#'
+#' @description Compute treatment effects in interactive fixed effects models
+#'  with a small number of time periods by exploiting staggered treatment
+#'  adoption
+#'
+#' @inheritParams pte:pte
+#' @param nife the number of interactive fixed effects to include in the model
+#'
+#' @return \code{pte::pte_results} object
+#'
+#' @export
+staggered_ife <- function(yname,
+                          gname,
+                          tname,
+                          idname,
+                          data,
+                          nife,
+                          xformla=~1,
+                          ret_ife_regs=TRUE,
+                          anticipation=0,
+                          cband=TRUE,
+                          alp=0.05,
+                          boot_type="multiplier",
+                          biters=100,
+                          cl=1) {
+  
+  # set this in order to use `setup_pte` function provided by `pte` package
+  required_pre_periods <- nife+1
+  
+  res <- pte(yname=yname,
+             gname=gname,
+             tname=tname,
+             idname=idname,
+             data=data,
+             setup_pte_fun=staggered_ife_setup_pte,
+             subset_fun=ife_subset,
+             attgt_fun=staggered_ife_attgt,
+             nife=nife,
+             xformla=xformla,
+             ret_ife_regs=ret_ife_regs,
+             required_pre_periods=required_pre_periods,
+             anticipation=anticipation,
+             cband=cband,
+             alp=alp,
+             boot_type=boot_type,
+             biters=biters,
+             cl=cl)
+
+  res
+}
